1efcce83ef2f9f059946785988264dcf88e8bea3
0xc010d <0xc010d@gmail.com>
2015-12-02 19:10:14 +0100

    added more additionalProperties tests

--- tests/AdditionalPropertiesTests.cpp
+++ tests/AdditionalPropertiesTests.cpp
@@ -8,7 +8,7 @@ TEST(AdditionalProperties, Object)
   const std::string json = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
   const std::string additionalProperties = R"({})";
   const std::string schema = R"({"additionalProperties": {}})";
-  ASSERT_EQ(v.validate_additionalItems(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
 }
 
 TEST(AdditionalProperties, Allowed)
@@ -17,5 +17,106 @@ TEST(AdditionalProperties, Allowed)
   const std::string json = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
   const std::string additionalProperties = R"(true)";
   const std::string schema = R"({"additionalProperties": true})";
-  ASSERT_EQ(v.validate_additionalItems(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+}
+
+TEST(AdditionalProperties, DisallowedNoPropertiesNoPatternProperties)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"({"additionalProperties": false})";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), false);
+}
+
+TEST(AdditionalProperties, DisallowedNoPropertiesSuccess)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"(
+{
+  "additionalProperties": false,
+  "patternProperties":
+  {
+    ".+": {}
+  }
+}
+)";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+}
+
+TEST(AdditionalProperties, DisallowedNoPropertiesFailure)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"1": 1, "2": 2, "3": 3, "p1": 1})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"(
+{
+  "additionalProperties": false,
+  "patternProperties":
+  {
+    ".": {}
+  }
+}
+)";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), false);
+}
+
+TEST(AdditionalProperties, DisallowedNoPatternPropertiesSuccess)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"p1": 1, "p2": 2, "p3": 3})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"(
+{
+  "additionalProperties": false,
+  "properties": {"p1": {}, "p2": {}, "p3": {}}
+}
+)";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+}
+
+TEST(AdditionalProperties, DisallowedNoPatternPropertiesFailure)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"(
+{
+  "additionalProperties": false,
+  "properties": {"p1": {}, "p2": {}, "p3": {}}
+}
+)";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), false);
+}
+
+TEST(AdditionalProperties, DisallowedSuccess)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"1": 1, "2": 2, "3": 3, "p1": 1, "p2": 2, "p3": 3})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"(
+{
+  "additionalProperties": false,
+  "patternProperties": {".": {}},
+  "properties": {"p1": {}, "p2": {}, "p3": {}}
+}
+)";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+}
+
+TEST(AdditionalProperties, DisallowedFaiure)
+{
+  ValidatorImpl v;
+  const std::string json = R"({"1": 1, "2": 2, "3": 3, "p1": 1, "p2": 2, "p3": 3, "p4": 4})";
+  const std::string additionalProperties = R"(false)";
+  const std::string schema = R"(
+{
+  "additionalProperties": false,
+  "patternProperties": {".": {}},
+  "properties": {"p1": {}, "p2": {}, "p3": {}}
+}
+)";
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), false);
 }
