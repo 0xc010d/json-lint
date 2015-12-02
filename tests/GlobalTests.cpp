6495c27dd7c4a1d0745fd438de33cc96c6bab0a7
0xc010d <0xc010d@gmail.com>
2015-12-02 20:42:48 +0100

    added json helper assert, fixed tests

--- tests/GlobalTests.cpp
+++ tests/GlobalTests.cpp
@@ -4,21 +4,21 @@
 
 TEST(Global, EmptySchema) {
   ValidatorImpl v;
-  ASSERT_EQ(v.validate("test"), true);
-  ASSERT_EQ(v.validate("{}"), true);
-  ASSERT_EQ(v.validate("[]"), true);
-  ASSERT_EQ(v.validate("true"), true);
-  ASSERT_EQ(v.validate("15"), true);
-  ASSERT_EQ(v.validate("[1,2,3,4.5,{\"key\": true}]"), true);
+  ASSERT_EQ(v.validate(_json_(R"("test")")), true);
+  ASSERT_EQ(v.validate(_json_(R"({})")), true);
+  ASSERT_EQ(v.validate(_json_(R"([])")), true);
+  ASSERT_EQ(v.validate(_json_(R"(true)")), true);
+  ASSERT_EQ(v.validate(_json_(R"(15)")), true);
+  ASSERT_EQ(v.validate(_json_(R"([1,2,3,4.5,{"key": true}])")), true);
 }
 
 TEST(Global, UnknownValidator) {
   const std::string schema = R"({"unknownValidator": {}})";
   ValidatorImpl v(_json_(schema));
-  ASSERT_EQ(v.validate("test"), true);
-  ASSERT_EQ(v.validate("{}"), true);
-  ASSERT_EQ(v.validate("[]"), true);
-  ASSERT_EQ(v.validate("true"), true);
-  ASSERT_EQ(v.validate("15"), true);
-  ASSERT_EQ(v.validate("[1,2,3,4.5,{\"key\": true}]"), true);
+  ASSERT_EQ(v.validate(_json_(R"("test")")), true);
+  ASSERT_EQ(v.validate(_json_(R"({})")), true);
+  ASSERT_EQ(v.validate(_json_(R"([])")), true);
+  ASSERT_EQ(v.validate(_json_(R"(true)")), true);
+  ASSERT_EQ(v.validate(_json_(R"(15)")), true);
+  ASSERT_EQ(v.validate(_json_(R"([1,2,3,4.5,{"key": true}])")), true);
 }
--- tests/_JsonHelper.h
+++ tests/_JsonHelper.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #import <json11.hpp>
+#import <assert.h>
 
 inline
 __attribute__((always_inline))
 json11::Json _json_(const std::string &string) {
   std::string error;
-  return json11::Json::parse(string, error);
+  auto json = json11::Json::parse(string, error);
+  assert(error.length() == 0);
+  return json;
 }
