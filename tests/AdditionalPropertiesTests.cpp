ceb5f8aaa5abaa4d4e14ad4d533db1e8e4b6d7a8
0xc010d <0xc010d@gmail.com>
2015-12-02 21:04:00 +0100

    improved additional properties tests

--- tests/AdditionalPropertiesTests.cpp
+++ tests/AdditionalPropertiesTests.cpp
@@ -32,18 +32,21 @@ TEST(AdditionalProperties, DisallowedNoPropertiesNoPatternProperties)
 TEST(AdditionalProperties, DisallowedNoPropertiesSuccess)
 {
   ValidatorImpl v;
-  const std::string json = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
+  const std::string json1 = R"({"p": 0, "p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
+  const std::string json2 = R"({"p1": 1, "p2": 2, "p3": 3, "p4": 4, "p5": 5})";
   const std::string additionalProperties = R"(false)";
   const std::string schema = R"(
 {
   "additionalProperties": false,
   "patternProperties":
   {
-    ".+": {}
+    ".+": {},
+    ".": {}
   }
 }
 )";
-  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json1), _json_(additionalProperties), _json_(schema)), true);
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json2), _json_(additionalProperties), _json_(schema)), true);
 }
 
 TEST(AdditionalProperties, DisallowedNoPropertiesFailure)
@@ -94,16 +97,21 @@ TEST(AdditionalProperties, DisallowedNoPatternPropertiesFailure)
 TEST(AdditionalProperties, DisallowedSuccess)
 {
   ValidatorImpl v;
-  const std::string json = R"({"1": 1, "2": 2, "3": 3, "p1": 1, "p2": 2, "p3": 3})";
+  const std::string json1 = R"({"1": 1, "2": 2, "3": 3, "p1": 1, "p2": 2, "p3": 3})";
+  const std::string json2 = R"({"1": 1, "2": 2, "3": 3, "p1": 1, "p2": 2, "p3": 3, "p4": 4, "ppp": ""})";
   const std::string additionalProperties = R"(false)";
   const std::string schema = R"(
 {
   "additionalProperties": false,
-  "patternProperties": {".": {}},
-  "properties": {"p1": {}, "p2": {}, "p3": {}}
+  "patternProperties": {
+    ".": {},
+    ".{3}": {}
+  },
+  "properties": {"p1": {}, "p2": {}, "p3": {}, "p4": {}}
 }
 )";
-  ASSERT_EQ(v.validate_additionalProperties(_json_(json), _json_(additionalProperties), _json_(schema)), true);
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json1), _json_(additionalProperties), _json_(schema)), true);
+  ASSERT_EQ(v.validate_additionalProperties(_json_(json2), _json_(additionalProperties), _json_(schema)), true);
 }
 
 TEST(AdditionalProperties, DisallowedFaiure)
